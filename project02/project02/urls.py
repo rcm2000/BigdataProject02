fe7709c7053c7809474d52dbe7195eb242616e9c
Cheong Myeong Noh <cjdauddl93@gmail.com>
2021-08-05 17:52:00 +0900

    build_bootstrap_day02

--- project02/project02/urls.py
+++ project02/project02/urls.py
@@ -21,6 +21,11 @@ from project02 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('index2', views.index2, name='index2'),
+    path('index3', views.index3, name='index3'),
+    path('index4', views.index4, name='index4'),
+    path('login', views.login, name='login'),
 
 
 ]
--- project02/project02/views.py
+++ project02/project02/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def index(request):
 
 	return render(request, 'index.html')
+def index2(request):
+
+	return render(request, 'index2.html')
+def index3(request):
+
+	return render(request, 'index3.html')
+def index4(request):
+
+	return render(request, 'index4.html')
+def login(request):
+
+	return render(request, 'login.html')
